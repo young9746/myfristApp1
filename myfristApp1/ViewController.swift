a97811eb839c971361f0a13395ebd4dfedb71014
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-03-08 11:27:41 +0900

    Initial Commit

---
+++ myfristApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfristApp1
+//
+//  Created by D7703_28 on 2018. 3. 8..
+//  Copyright © 2018년 KimYoungSeok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
